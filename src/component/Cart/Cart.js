e1de8b3c849d7ec92922872c61e2478ad8bce9e0
HasanShaikat <hasanshaikat9@gmail.com>
2022-06-22 17:59:14 +0600

    update cart list

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({cart}) => {
-  
+
+
 
     return (
+
         <div className="cart">
-           
             <div className="ui-name-show">
                 <ul>
                     <h3 >Cart List</h3>
@@ -16,8 +17,9 @@ const Cart = ({cart}) => {
                     }
                 </ul>
             </div>
-
-            <button >Select One</button>
+            <div className="cartBtn">
+                <button>Choioce 1 for me</button>
+            </div>
         </div>
     );
 };
